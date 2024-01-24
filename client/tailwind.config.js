fb8c2bdeafe1939619ebb3f4147b4ee0da223278
ggsno <ogs0518@gmail.com>
2024-01-24 23:09:40 +0900

    chore: tailwindcss 설정 변경 및 예시 파일 삭제

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import colors from "tailwindcss/colors";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      ...colors,
-      point: "#73060F",
-      "snow-white": "#EBEFF2",
-      charcoal: "#36454F",
+    extend: {
+      colors: {
+        point: "#73060F",
+        "snow-white": "#EBEFF2",
+        charcoal: "#36454F",
+      },
     },
   },
   plugins: [],
